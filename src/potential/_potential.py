de12570ff3eddf7130f4406af734f3a89b90297a
Harry Saunders <hsaunders1904@icloud.com>
2021-04-11 00:46:59 +0100

    Remove unused _potential.py file

--- src/potential/_potential.py
+++
@@ -1,7 +0,0 @@
-from ._cpp import potential_cpp
-from ._numpy import potential_np
-from ._py import potential_py
-from ._numba import potential_numba
-from ._cython import potential_cython
-from ._cl import potential_cl_cpu, potential_cl_gpu
-from ._cuda import potential_cuda
