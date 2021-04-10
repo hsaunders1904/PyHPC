f73be97d9686ce7b18d2899d5a435f181d8214b8
Harry Saunders <hsaunders1904@icloud.com>
2021-04-11 00:58:51 +0100

    Add gen_charges to utils.py
    Also removed duplicated "potential_np" function in the same file

--- src/potential/utils.py
+++ src/potential/utils.py
@@ -15,18 +15,5 @@ def gen_particles(N, dist="circle", dtype="float64"):
         raise ValueError(f"Unrecognised distribution {dist}.")
 
 
-def potential_np(particle_coords, grid_resolution, charges):
-    # Create mesh xx, yy
-    x = np.linspace(0, 1, grid_resolution)
-    xx, yy = np.meshgrid(x, x)
-
-    # Create matrix of zeros
-    potential_grid = np.zeros((grid_resolution, grid_resolution))
-
-    # Increment the matrix for each particle
-    for i, coords in enumerate(particle_coords):
-        delta_x = np.square(xx - coords[0])
-        delta_y = np.square(yy - coords[1])
-        distance = np.sqrt(delta_x + delta_y)
-        potential_grid -= charges[i]*np.log(distance)
-    return potential_grid
+def gen_charges(N):
+    return np.random.choice([-1, 1], (N, )).astype("int32")
