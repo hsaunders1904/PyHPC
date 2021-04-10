074d7a4247a8d72536b8792a861fe4b60f43fe71
Harry Saunders <hsaunders1904@icloud.com>
2021-04-10 23:18:28 +0100

    Compile C++ library in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,54 @@
+import os
+import subprocess
+import sys
 from Cython.Build import cythonize
 from setuptools import find_packages, setup, extension
+from setuptools.command.build_ext import build_ext as build_ext_orig
 
 import numpy as np
+import pybind11
 
-extensions = extension.Extension(
+cython_ext = extension.Extension(
     "potential._cython",
-    ["src/potential/_cython.pyx"],
+    [os.path.join(os.path.dirname(__file__), "src/potential/_cython.pyx")],
     include_dirs=[np.get_include()],
 )
+
+
+class ExtensionBuilder(build_ext_orig):
+    """
+    Extend the :class:`build_ext_orig` to build our C++ library with
+    CMake.
+    """
+
+    def run(self):
+        super().run()
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        import cmake  # import here so install_requires runs first
+
+        cmake_path = os.path.join(cmake.CMAKE_BIN_DIR, "cmake")
+        ext_dir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name))
+        )
+
+        configure_args = [
+            "-S",
+            os.path.dirname(__file__), "-B", self.build_temp,
+            f"-DPython_EXECUTABLE=\"{sys.executable}\"",
+            "-DCMAKE_BUILD_TYPE=Release",
+            f"-Dpybind11_ROOT={pybind11.get_cmake_dir()}",
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={ext_dir}"
+        ]
+        build_args = ["--build", self.build_temp, "--config", "Release"]
+
+        print(" ".join([cmake_path, *configure_args]))
+        subprocess.check_call([cmake_path, *configure_args])
+        print(" ".join([cmake_path, *build_args]))
+        subprocess.check_call([cmake_path, *build_args])
+
 
 setup(
     name="potential",
@@ -15,5 +56,7 @@ setup(
     url="https://github.com/hsaunders1904/PyHPC",
     packages=find_packages(exclude=["*tests*"]),
     package_dir={"": "src"},
-    ext_modules=cythonize(extensions)
+    ext_modules=cythonize(cython_ext),
+    cmdclass={"build_ext": ExtensionBuilder},
+    setup_requires=["cmake", "cython", "numpy", "pybind11"]
 )
