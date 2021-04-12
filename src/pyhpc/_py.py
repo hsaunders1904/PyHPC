955e7a7627d2abb9dc5f9c2c31884fed17498ef5
Harry Saunders <hsaunders1904@icloud.com>
2021-04-12 13:30:58 +0100

    Avoid repeated calculation in _py.py

--- src/pyhpc/_py.py
+++ src/pyhpc/_py.py
@@ -19,9 +19,10 @@ def V_potential(grid_pos, particle_coords, k):
 def potential_py(particle_coords, grid_resolution, charges):
     potential_grid = np.zeros((grid_resolution, grid_resolution))
 
+    grid_step_denom = (grid_resolution - 1)
     for i in range(grid_resolution):
         for j in range(grid_resolution):
-            grid_pos = [i/(grid_resolution - 1), j/(grid_resolution - 1)]
+            grid_pos = [i/grid_step_denom, j/grid_step_denom]
             potential_grid[
                 j, i] = V_potential(grid_pos, particle_coords, charges)
     return potential_grid
