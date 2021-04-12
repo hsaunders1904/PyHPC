114fac20d09c5fb1ce17318b16a402e6aff0b196
Harry Saunders <hsaunders1904@icloud.com>
2021-04-12 09:34:08 +0100

    Use prange in calculate_grid numba function

--- src/potential/_numba.py
+++ src/potential/_numba.py
@@ -2,7 +2,7 @@ import numba
 import numpy as np
 
 
-@numba.jit(nopython=True)
+@numba.jit(nopython=True, parallel=True)
 def potential_numba(particle_coords, grid_resolution, charges):
     potential_grid = np.zeros((grid_resolution, grid_resolution))
 
@@ -13,10 +13,10 @@ def potential_numba(particle_coords, grid_resolution, charges):
     xx = yy.T
 
     # Increment the matrix for each particle
-    for coords, charge in zip(particle_coords, charges):
-        delta_x = np.square(xx - coords[0])
-        delta_y = np.square(yy - coords[1])
+    for i in numba.prange(len(charges)):  # note TBB is required for prange
+        delta_x = np.square(xx - particle_coords[i, 0])
+        delta_y = np.square(yy - particle_coords[i, 1])
         distance = np.sqrt(delta_x + delta_y)
-        potential_grid -= charge*np.log(distance)
+        potential_grid -= charges[i]*np.log(distance)
 
     return potential_grid
