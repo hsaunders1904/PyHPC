8e3dd6347d17fb7fb727d4f1df146f52c00fef6d
Harry Saunders <hsaunders1904@icloud.com>
2021-04-26 14:37:18 +0100

    Improve performance of numpy implementation

--- src/pyhpc/potential/_calculate_grid_impls/_numpy.py
+++ src/pyhpc/potential/_calculate_grid_impls/_numpy.py
@@ -6,12 +6,15 @@ def potential_np(particle_coords, grid_resolution, charges):
     x = np.linspace(0, 1, grid_resolution)
     xx, yy = np.meshgrid(x, x)
 
+    # Allocating an empty array and using the "out" kwarg prevents
+    # creation of new objects, which boosts performance
+    field_increment = np.empty((grid_resolution, grid_resolution))
+
     potential_grid = np.zeros((grid_resolution, grid_resolution))
     # Increment the grid for each particle
     for coords, charge in zip(particle_coords, charges):
-        delta_x = np.square(xx - coords[0])
-        delta_y = np.square(yy - coords[1])
-        distance = np.sqrt(delta_x + delta_y)
-        potential_grid -= charge*np.log(distance)
-
+        np.hypot(xx - coords[0], yy - coords[1], out=field_increment)
+        np.log(field_increment, out=field_increment)
+        field_increment *= charge
+        potential_grid -= field_increment
     return potential_grid
